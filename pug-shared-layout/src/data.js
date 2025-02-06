c4d367ba43c76df20b1071c16f1178aeda006cf1
Josh Archer <jarcher@greenriver.edu>
2025-02-06 11:05:26 -0800

    Added examples of loops, conditionals, and shared layouts.

---
+++ pug-shared-layout/src/data.js
@@ -0,0 +1,139 @@
+const books = [
+    {
+        "title": "Project Hail Mary",
+        "author": "Andy Weir",
+        "pages": 496,
+        "synopsis": "A scientist wakes up on a spaceship with no memory and must figure out how to save Earth from an impending disaster.",
+        "genre": "Science Fiction",
+        "publication_year": 2021,
+        "ISBN": "9780593135204"
+    },
+    {
+        "title": "The Way of Kings",
+        "author": "Brandon Sanderson",
+        "pages": 1007,
+        "synopsis": "An epic fantasy set in the storm-wracked world of Roshar, where powerful knights wield mystical Shardblades.",
+        "genre": "Fantasy",
+        "publication_year": 2010,
+        "ISBN": "9780765326355"
+    },
+    {
+        "title": "The Expanse: Leviathan Wakes",
+        "author": "James S.A. Corey",
+        "pages": 592,
+        "synopsis": "A missing girl, a derelict spaceship, and a conspiracy that threatens the entire solar system.",
+        "genre": "Science Fiction",
+        "publication_year": 2011,
+        "ISBN": "9780316129084"
+    },
+    {
+        "title": "Children of Time",
+        "author": "Adrian Tchaikovsky",
+        "pages": 600,
+        "synopsis": "A human colony ship discovers an alien civilization evolving in unexpected ways on a terraformed planet.",
+        "genre": "Science Fiction",
+        "publication_year": 2015,
+        "ISBN": "9781447273288"
+    },
+    {
+        "title": "The Blacktongue Thief",
+        "author": "Christopher Buehlman",
+        "pages": 416,
+        "synopsis": "A thief with a debt to the guild, a warrior with a deadly mission, and a dangerous journey through a land scarred by war and magic.",
+        "genre": "Fantasy",
+        "publication_year": 2021,
+        "ISBN": "9781250621191"
+    },
+    {
+        "title": "Empire of Silence",
+        "author": "Christopher Ruocchio",
+        "pages": 624,
+        "synopsis": "A man who refuses to be a hero is instead remembered as a galaxy-destroying monster in this epic space opera.",
+        "genre": "Science Fiction",
+        "publication_year": 2018,
+        "ISBN": "9780756413002"
+    },
+    {
+        "title": "The Poppy War",
+        "author": "R.F. Kuang",
+        "pages": 544,
+        "synopsis": "A war orphan wins entrance to an elite military school and discovers she has a powerful and dangerous gift.",
+        "genre": "Fantasy",
+        "publication_year": 2018,
+        "ISBN": "9780062662569"
+    },
+    {
+        "title": "The Dresden Files: Peace Talks",
+        "author": "Jim Butcher",
+        "pages": 352,
+        "synopsis": "Chicago’s only wizard-for-hire is caught between supernatural factions when peace talks break down.",
+        "genre": "Urban Fantasy",
+        "publication_year": 2020,
+        "ISBN": "9780451464415"
+    },
+    {
+        "title": "The Rage of Dragons",
+        "author": "Evan Winter",
+        "pages": 544,
+        "synopsis": "A young man seeks revenge in a war-torn kingdom where some people are gifted with godlike powers.",
+        "genre": "Fantasy",
+        "publication_year": 2019,
+        "ISBN": "9780316489768"
+    },
+    {
+        "title": "Dark Matter",
+        "author": "Blake Crouch",
+        "pages": 342,
+        "synopsis": "A physicist is abducted into an alternate reality where he made drastically different life choices.",
+        "genre": "Science Fiction",
+        "publication_year": 2016,
+        "ISBN": "9781101904220"
+    },
+    {
+        "title": "The Shadow of the Gods",
+        "author": "John Gwynne",
+        "pages": 528,
+        "synopsis": "In a world shaped by the fall of ancient gods, warriors and mercenaries seek power and revenge.",
+        "genre": "Fantasy",
+        "publication_year": 2021,
+        "ISBN": "9780316539883"
+    },
+    {
+        "title": "Iron Gold",
+        "author": "Pierce Brown",
+        "pages": 624,
+        "synopsis": "The rebellion has won, but the struggle to maintain a new order in the solar system is only beginning.",
+        "genre": "Science Fiction",
+        "publication_year": 2018,
+        "ISBN": "9780425285916"
+    },
+    {
+        "title": "A Little Hatred",
+        "author": "Joe Abercrombie",
+        "pages": 471,
+        "synopsis": "The industrial revolution brings new challenges to a world of war, intrigue, and ruthless ambition.",
+        "genre": "Fantasy",
+        "publication_year": 2019,
+        "ISBN": "9780316187169"
+    },
+    {
+        "title": "The Pariah",
+        "author": "Anthony Ryan",
+        "pages": 592,
+        "synopsis": "A thief and outlaw is drawn into a world of war, treachery, and deadly secrets.",
+        "genre": "Fantasy",
+        "publication_year": 2021,
+        "ISBN": "9780316430778"
+    },
+    {
+        "title": "The Ember Blade",
+        "author": "Chris Wooding",
+        "pages": 832,
+        "synopsis": "A classic fantasy adventure of revolution, exile, and a magical blade that could change the fate of the world.",
+        "genre": "Fantasy",
+        "publication_year": 2018,
+        "ISBN": "9781473214845"
+    }
+]
+
+export default books;
