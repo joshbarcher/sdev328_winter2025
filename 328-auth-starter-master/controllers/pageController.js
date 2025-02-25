76bde939dfce281ca4c8f77985a283ced7c0dc25
Josh Archer <jarcher@greenriver.edu>
2025-02-25 11:07:38 -0800

    Added authorization steps.

--- 328-auth-starter-master/controllers/pageController.js
+++ 328-auth-starter-master/controllers/pageController.js
@@ -1,9 +1,15 @@
 
 const userPage = (req, res) => {
-    res.status(200).render("user", {})
+    console.log(req.user.username);
+    if (req.user) {
+        res.status(200).render("user", {});
+    } else {
+        res.redirect("/login");
+    }
 }
 
 const adminPage = (req, res) => {
+    console.log(req.user.username);
     res.status(200).render("admin", {})
 }
 
