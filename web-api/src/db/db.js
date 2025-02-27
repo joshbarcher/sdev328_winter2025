2eedd265048bc75e1d765acdc66e34660c5d48ea
Josh Archer <jarcher@greenriver.edu>
2025-02-27 11:28:51 -0800

    Added starter files.

---
+++ web-api/src/db/db.js
@@ -0,0 +1,25 @@
+import { Sequelize } from "sequelize";
+import chalk from 'chalk';
+
+const host = "localhost";
+const port = 3307;
+const database = "recipes";
+const user = "jarcher";
+const password = "F6CraKpFjfbIo1";
+
+const sequelize = new Sequelize(database, user, password, {
+    host,
+    port,
+    dialect: 'mysql',
+    logging: msg => console.log(`SQL: ${chalk.blue(msg)}`)
+})
+
+try {
+    //try to connect!
+    await sequelize.authenticate();
+    console.log("Connect to DB!");
+} catch (err) {
+    console.log(`Unable to connect to DB: ${err}`);
+}
+
+export default sequelize;
